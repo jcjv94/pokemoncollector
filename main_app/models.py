121c1ff16dc106a6a5f5d353e1eece97f7f14a31
Jon Carlo Vargas <jcjv94@gmail.com>
2019-12-13 16:01:11 -0800

    complete lab friday 13th

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Pokemon(models.Model):
+    name = models.CharField(max_length=100)
+    attribute = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    level = models.IntegerField()
+    
+    def __str__(self):
+        return self.name
+
+
+
--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('pokemons/', views.pokemons_index, name='index'),
+    path('pokemons/<int:pokemon_id>/', views.pokemons_detail, name='detail')
 ]
